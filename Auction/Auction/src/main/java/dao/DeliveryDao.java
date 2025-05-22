977201990cd4c42be4c3c1a3364eb9f804f04ea3
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2025-05-23 07:29:14 +0900

    Delete Auction directory

--- Auction/Auction/src/main/java/dao/DeliveryDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import domain.Delivery;
-
-public interface DeliveryDao {
-    Delivery findByOrderId(Long orderId);
-    void insertDelivery(Delivery delivery);
-    void updateDelivery(Delivery delivery);
-}
--- Auction/src/main/java/dao/InterestDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public interface InterestDao {
-    List<Interest> findByUserId(String userId);
-    void insertInterest(Interest interest);
-    void deleteInterest(Long interestId);
-}
-
